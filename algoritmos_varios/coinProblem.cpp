8c8dd2b80b7b94089a269d4c0b3331dd58527d64
JuanVaTe <jcvate2013@hotmail.com>
2021-09-21 18:59:00 -0500

    Finalización del algoritmo Coin Problem

--- algoritmos_varios/coinProblem.cpp
+++ algoritmos_varios/coinProblem.cpp
@@ -63,39 +63,131 @@ double eps = 1e-12;
 
 //Plantilla base para programación competitiva --> https://www.geeksforgeeks.org/how-to-setup-competitive-programming-in-visual-studio-code-for-c/
 
-v32 greedyCoinP(v32 v, int r){
-    v32 vr;
+//Algoritmo avaro
+v64 greedyCoinP(v64 v, int r){
+    //Instanciación de variables
+    //Vector donde se guardan las monedas
+    v64 vr;
+
+    //Total y apuntador de monedas
     int t = 0; int ap = 0;
+
+    //Ordenamiento de mayor a menor para algoritmo avaro
     sort(all(v));
     reverse(all(v));
+
+    //Mientras el total sea menor que el objetivo
     while(t < r){
+
+        //Validación de la posibilidad de conseguir el objetivo
         if(t + v[ap] > r){
+
+            //Cambia la moneda
             ap++;
+
+            //Corta si ya no hay más monedas
             if(ap == sz(v)) break;
             continue;
         }
+
+        //Inserción de monedas
         vr.pb(v[ap]);
+
+        //Actualización del total
         t += v[ap];
     }
+
+    //Regresa las monedas
     return vr;
 }
 
-int main()
-{
+//Algoritmo con programación dinámica
+v64 DPCoinP(v64 v, int r){
+
+    //Instanciación de variables
+    //Vector donde se guardan el menor número de monedas necesarias de cada valor
+    v64 memo(r+1);
+
+    //Vector donde se guarda la primera moneda utilizada en cada valor/Vector donde se guardan las moendas
+    v64 mon(r+1), vr;
+
+    //0 = 0 monedas
+    memo[0] = 0;
+
+    //Cálculo del mínimo de monedas de cada elemento hasta que llegue al objetivo
+    forsn(i, 1, r+1){
+
+        //Infinito para que la primera comparación siempre sea menor
+        memo[i] = INF;
+
+        //Elección de la primera moneda en cada valor
+        forn(j, sz(v)){
+
+            //Valida si se puede utilizar la moneda elegida y si es más optimo que la opción anterior
+            if((i-v[j] >= 0) && (memo[i-v[j]]+1 < memo[i])){
+
+                //Actualización del valor óptimo
+                memo[i] = memo[i-v[j]] + 1;
+
+                //Actualización de la primera moneda utilizada
+                mon[i] = v[j];
+            }
+        }
+    }
+
+    //Inserción de las monedas utilizadas
+    while(r > 0){
+        //Inserta la primera moneda utilizada en r
+        vr.pb(mon[r]);
+
+        //Resta el valor de la moneda a r
+        r -= mon[r];
+    }
+
+    //Ordenamiento del vector
+    sort(all(vr));
+    reverse(all(vr));
+
+    //Regresa el vector
+    return vr;
+}
+
+//Función main
+int main(){
+
+    //Optimización de streams
     fast_cin();
+
+    //Instanciación de variables
+    //Total de monedas/Cin de monedas/Precio/Pago/Cambio
     int t, x, p, q, r;
-    v32 v, vg, vdp;
+
+    //Vector de monedas/Vector de monedas avaro/Vector de monedas DP
+    v64 v, vg, vdp;
+
+    //Obtiene el total de monedas
     cin >> t;
+
+    //Inserción de monedas en vector
     for(int it=1;it<=t;it++) {
         cin >> x;
         v.pb(x);
     }
 
+    //Obtiene precio y pago
     cin >> p;
     cin >> q;
 
+    //Cálculo del cambio
     r = q - p;
 
+    //Impresión del vector del algoritmo DP
+    vdp = DPCoinP(v, r);
+    for (auto x : vdp){
+        cout << x << ln;
+    }
+
+    //Impresión del vector de algoritmo avaro
     vg = greedyCoinP(v, r);
     for (auto x : vg){
         cout << x << ln;
