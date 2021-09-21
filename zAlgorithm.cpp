b60789547ced362521a47fb3c72f9b54c6223699
JuanVaTe <jcvate2013@hotmail.com>
2021-09-21 15:45:05 -0500

    Finalizado el zAlgorithm y añadidas configuraciones para ignorar ejecutables (C++)

--- zAlgorithm.cpp
+++ zAlgorithm.cpp
@@ -1,6 +1,9 @@
+//Configuracion del programa
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
+
+//Librerías
 #include <complex>
 #include <queue>
 #include <set>
@@ -20,7 +23,8 @@
 #include <fstream>
  
 using namespace std;
- 
+
+//Definición de datatypes
 typedef long long ll;
 typedef long double ld;
 typedef pair<int,int> p32;
@@ -33,8 +37,12 @@ typedef vector<vector<ll> > vv64;
 typedef vector<vector<p64> > vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
+
+//Definición de constantes
 ll MOD = 998244353;
 double eps = 1e-12;
+
+//Definición de macros
 #define forn(i,e) for(ll i = 0; i < e; i++)
 #define forsn(i,s,e) for(ll i = s; i < e; i++)
 #define rforn(i,s) for(ll i = s; i >= 0; i--)
@@ -49,18 +57,71 @@ double eps = 1e-12;
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((ll)(x).size())
- 
 
-void solve(){
+//Plantilla base para programación competitiva --> https://www.geeksforgeeks.org/how-to-setup-competitive-programming-in-visual-studio-code-for-c/
+
+v32 zAlgorithm(string S){
+    //Instanciación de variables
+    v32 Z(sz(S), 0);
+    Z[0] = 0;
+    int l = 0; int r = 0; int j = 0;
+
+    //Recorrido del string
+    forsn(i, 1, sz(S)){
+
+        //Validación si esta dentro o fuera de la caja.
+        //Fuera
+        if(i > r){
+            //Actualización de los lados de la caja.
+            r = l = i;
+
+            //Validación inbounds e igualdad de prefijo.
+            while((r < sz(S)) && (S[r] == S[r-i])) r++;
+
+            //Inserción del resultado.
+            Z[i] = r - l;
+            r--;
+        }
+        //Dentro
+        else{
+            //Apuntador hacia los elementos ya calculados
+            j = i - l;
+
+            //Valida si es necesario mover la caja
+            if(i + Z[j] < r) Z[i] = Z[j];
+            else{
+                //Actualización del lado izquierdo de la caja
+                l = i;
+
+                //Validación inbounds e igualdad de prefijo.
+                while((r < sz(S)) && (S[r] == S[r-i])) r++;
+
+                //Inserción del resultado.
+                Z[i] = r - l;
+                r--;
+            }
+        }
+    }
+    //Regresa resultado
+    return Z;
 }
+
 int main()
 {
+    //Streams rápidos
     fast_cin();
-    ll t;
-    cin >> t;
-    for(int it=1;it<=t;it++) {
-     cout << "Case #" << it+1 << ": ";
-        solve();
+
+    //Obtiene S
+    string S;
+    cin >> S;
+
+    //Llamado a la función
+    v32 Z = zAlgorithm(S);
+
+    //Impresión del resultado
+    for(auto x : Z){
+        cout << x << " ";
     }
+
     return 0;
 }
